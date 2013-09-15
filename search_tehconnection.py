22d4e5323480df18237df91748789d998de8eaee
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2013-09-15 23:33:21 +0200

    Converted tabs to space

--- search_tehconnection.py
+++ search_tehconnection.py
@@ -8,32 +8,32 @@ from flexget.utils.search import torrent_availability
 log = logging.getLogger('search_tehconnection')
 
 class SearchTC(object):
-	def search(self, entry, config):
-		url = "https://tehconnection.eu/torrents.php?searchstr=%s" \
-			% entry.get("imdb_id")
+  def search(self, entry, config):
+    url = "https://tehconnection.eu/torrents.php?searchstr=%s" \
+      % entry.get("imdb_id");
 
-		page = urlopener(url, log)
-		soup = get_soup(page)
+    page = urlopener(url, log)
+    soup = get_soup(page)
 
-		results = set()
+    results = set()
 
-		for row in soup.find_all("tr", class_="group_torrent"):
-			link = row.find(title="Download")
-			info = row.find(colspan="1").contents[3].contents[0].strip()
-			seeders = int(row.find_all("td")[6].contents[0].strip())
-			leechers = int(row.find_all("td")[7].contents[0].strip())
+    for row in soup.find_all("tr", class_="group_torrent"):
+      link = row.find(title="Download")
+      info = row.find(colspan="1").contents[3].contents[0].strip()
+      seeders = int(row.find_all("td")[6].contents[0].strip())
+      leechers = int(row.find_all("td")[7].contents[0].strip())
 
-			result = Entry()
-			result["title"] = entry.get("title") + " / " + info
-			result["imdb_id"] = entry.get("imdb_id");
-			result["url"] = "https://tehconnection.eu" + link.get("href")
-			result["torrent_seeds"] = seeders
-			result["torrent_leeches"] = leechers
-			result["search_sort"] = torrent_availability(result['torrent_seeds'],
-																									 result['torrent_leeches'])
+      result = Entry();
+      result["title"] = entry.get("title") + " / " + info
+      result["imdb_id"] = entry.get("imdb_id")
+      result["url"] = "https://tehconnection.eu" + link.get("href")
+      result["torrent_seeds"] = seeders
+      result["torrent_leeches"] = leechers
+      result["search_sort"] = torrent_availability(result['torrent_seeds'],
+                                                   result['torrent_leeches'])
 
-			results.add(result)
+      results.add(result)
 
-		return results
+    return results
 
 register_plugin(SearchTC, 'search_tehconnection', groups=['search'], debug=True)
