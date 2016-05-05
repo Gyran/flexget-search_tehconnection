3388dc1c9ad13f4aadea69212895a7d5859a6ade
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2016-05-05 08:41:22 +0200

    Plugin works with Flexget 2

--- search_tehconnection.py
+++ search_tehconnection.py
@@ -1,23 +1,21 @@
 import logging
-from flexget.entry import Entry
+
 from flexget import plugin
+from flexget.entry import Entry
 from flexget.event import event
-from flexget.utils.tools import urlopener
 from flexget.utils.soup import get_soup
 from flexget.utils.search import torrent_availability
 
 log = logging.getLogger('search_tehconnection')
 
 class SearchTC(object):
-  def search(self, entry, config):
-    url = "https://tehconnection.eu/torrents.php?searchstr=%s" \
-      % entry.get("imdb_id");
+  def search(self, task, entry, config=None):
 
-    page = urlopener(url, log)
+    url = "https://tehconnection.eu/torrents.php?searchstr=" + entry["imdb_id"];
+    page = task.requests.get(url).content
     soup = get_soup(page)
 
     results = set()
-
     for row in soup.find_all("tr", class_="group_torrent"):
       link = row.find(title="Download")
       info = row.find(colspan="1").contents[3].contents[0].strip()
@@ -25,8 +23,8 @@ class SearchTC(object):
       leechers = int(row.find_all("td")[7].contents[0].strip())
 
       result = Entry();
-      result["title"] = entry.get("title") + " / " + info
-      result["imdb_id"] = entry.get("imdb_id")
+      result["title"] = entry["title"] + " / " + info
+      result["imdb_id"] = entry["imdb_id"]
       result["url"] = "https://tehconnection.eu" + link.get("href")
       result["torrent_seeds"] = seeders
       result["torrent_leeches"] = leechers
@@ -39,4 +37,4 @@ class SearchTC(object):
 
 @event('plugin.register')
 def register_plugin():
-  plugin.register(SearchTC, 'search_tehconnection', groups=['search'], api_ver=2, debug=True)
+  plugin.register(SearchTC, 'search_tehconnection', groups=['search'], api_ver=2)
